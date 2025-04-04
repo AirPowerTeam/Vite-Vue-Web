d70e3a676638bec8f78f5dd9874ccf19d7f639a8
Hamm <majhamm@qq.com>
2025-04-04 22:30:05 +0800

    refactor(src): 重构表格组件并更新依赖
    - 更新 @airpower/web 依赖至 0.0.37 版本
    - 重构 App.vue 中的表格组件使用方式
    - 调整导入语句和组件使用

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { UserEntity } from "./model/UserEntity";
-import { UserService } from "./model/UserService";
-import { useTable, WebTable } from "@airpower/web";
+import {UserEntity} from "./model/UserEntity";
+import {UserService} from "./model/UserService";
+import {TableController, WebTable} from "@airpower/web";
 
-const { response, isLoading } = useTable(UserEntity, UserService);
+// const { response, isLoading } = useTable(UserEntity, UserService);
+const {response, isLoading} = new TableController(UserService)
 </script>
 
 <template>
   <div class="table">
-    <WebTable v-loading="isLoading" :data="response.list" :clazz="UserEntity">
-    </WebTable>
+    <WebTable v-loading="isLoading" :data="response.list" :clazz="UserEntity"/>
   </div>
 </template>
 
