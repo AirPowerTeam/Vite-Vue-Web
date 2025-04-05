c55cf9716c1133d03acafc0951373da9ca7d939c
Hamm <majhamm@qq.com>
2025-04-05 22:23:49 +0800

    fix(bugs): 修复了一些已知的BUG

--- src/model/UserEntity.ts
+++ src/model/UserEntity.ts
@@ -1,17 +1,38 @@
-import { AirEntity, Field, Table } from "@airpower/web"
+import { AirEntity, Field, Table } from "@airpower/web";
+import { UserGender } from "./UserGender";
 
 export class UserEntity extends AirEntity {
-  @Table()
-  @Field({
-    label: "邮箱"
-  })
-  email!: string
+  // @Field({}) @Table({}) 都是装饰器
+  // 生成用户的属性 包括 昵称 密码 邮箱 手机号 身份证号 头像 性别等字段，并分别标记好装饰器
 
-  password!: string
+  // 使用 !断言属性 不要给默认值
+  @Field({})
+  @Table({})
+  nickname!: string;
+
+  @Field({})
+  @Table({})
+  password!: string;
+
+  @Field({})
+  @Table({})
+  email!: string;
+
+  @Field({})
+  @Table({})
+  phone!: string;
+
+  @Field({})
+  @Table({})
+  idCard!: string;
+
+  @Field({})
+  @Table({})
+  avatar!: string;
 
   @Field({
-    label: "昵称"
+    type: UserGender,
   })
-  @Table()
-  nickname!: string
+  @Table({})
+  gender!: UserGender;
 }
